cc8ab36d452430ef250530863bee45ada06d698b
chrisaberson <cla18@humboldt.edu>
2020-06-15 13:55:27 -0700

    Add files via upload

---
+++ R/IntroTutorials.R
@@ -0,0 +1,13 @@
+#' Tutorials for Introductory Statistics
+#'
+#' @description
+#'
+#' This package supports online teaching for teaching introductory stats
+#'
+#' @import ggplot2 car lsr apaTables BayesFactor tidyverse
+#' @docType package
+#' @name IntroTutorials
+#'
+#'
+#'
+NULL
---
+++ R/data.R
@@ -0,0 +1,56 @@
+#' Class Data 2018
+#'
+#' Psych 241 Class Data (Fall18)
+#'
+#'
+#' @format A data frame with 115 rows and 13 variables:
+#' \describe{
+#'   \item{ID}{Identification Number}
+#'   \item{Class_Rank}{Factor with four levels. Frosh, Sophmore, Junior, Senior}
+#'   \item{GPA}{College Grade Point Average}
+#'   \item{Major}{Current Major with two levels. Psychology and Not Psychology}
+#'   \item{Psych_Courses}{Number of Psychology Courses Completed}
+#'   \item{Study_Hours}{Hours studied per week}
+#'   \item{Tv_hours}{Hours watching tv per week}
+#'   \item{Summer_Tv}{Hours watching tv per week during Summer}
+#'   \item{FirstGen}{Are you a first generation student? Coded as Yes, No}
+#'   \item{Siblings}{Number of Siblings}
+#'   \item{Grad_School}{Intentions to attend graduate school. Coded as Yes, No}
+#'   \item{Expected_Grade}{Grade expected for class}
+#'   \item{HS_GPA}{High School Grade Point Average}
+#'
+
+"Fall18"
+
+#' Lab 3 Data
+#'
+#'
+#' @format A data frame with 252 rows and 33 variables:
+#' \describe{
+#'   \item{weight}{Participant's Weight in lbs}
+#'   \item{height}{Participant's Height in inches}
+#'   \item{age}{Participants age in years}
+#'
+
+"Lab3"
+
+
+#' Climate Change Denial
+#'
+#'
+#' @format A data frame with 1587 rows and 11 variables:
+#' \describe{
+#'   \item{CCD}{Climate Change Denial}
+#'   \item{ANITESTABL}{Anti-Establishment Beliefs}
+#'   \item{EXCL_ANTIEG}{Anti-Egalitarian Preferences}
+#'   \item{TRADVALUE}{Traditional Values}
+#'   \item{OPENNESS}{Openness}
+#'   \item{PSEUDOSCI}{Pseudoscience beliefs}
+#'   \item{AGREEABL}{Agreeableness}
+#'   \item{Age}{Age in yeaers}
+#'   \item{Gender}{Gender}
+#'   \item{Education}{Education = Five Categories}
+#'   \item{ed}{Education = Two Categories}
+#'
+
+"denial"
