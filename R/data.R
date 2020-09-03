cbb9bf1602bd5f2c47fd8cbc54ff66939074371a
chrisaberson <cla18@humboldt.edu>
2020-09-02 18:06:57 -0700

    Add files via upload

--- R/data.R
+++ R/data.R
@@ -1,42 +1,3 @@
-#' Class Data 2018
-#'
-#' Psych 241 Class Data (Fall18)
-#'
-#' Class dataset.
-#'
-#' @format A data frame with 115 rows and 13 variables:
-#' \describe{
-#'   \item{ID}{Identification Number}
-#'   \item{Class_Rank}{Factor with four levels. Frosh, Sophmore, Junior, Senior}
-#'   \item{GPA}{College Grade Point Average}
-#'   \item{Major}{Current Major with two levels. Psychology and Not Psychology}
-#'   \item{Psych_Courses}{Number of Psychology Courses Completed}
-#'   \item{Study_Hours}{Hours studied per week}
-#'   \item{Tv_hours}{Hours watching tv per week}
-#'   \item{Summer_TV}{Hours watching tv per week during Summer}
-#'   \item{FirstGen}{Are you a first generation student? Coded as Yes, No}
-#'   \item{Siblings}{Number of Siblings}
-#'   \item{Grad_School}{Intentions to attend graduate school. Coded as Yes, No}
-#'   \item{Expected_Grade}{Grade expected for class}
-#'   \item{HS_GPA}{High School Grade Point Average}
-#'}
-
-"Fall18"
-
-#' Lab 3 Data
-#'
-#' Height, weight, and age data.
-#'
-#' @format A data frame with 252 rows and 33 variables:
-#' \describe{
-#'   \item{weight}{Participant's Weight in lbs}
-#'   \item{height}{Participant's Height in inches}
-#'   \item{age}{Participants age in years}
-#'}
-
-"Lab3"
-
-
 #' Climate Change Denial
 #'
 #' A dataset examining climate change denial and personality characteristics
@@ -54,6 +15,20 @@
 #'   \item{Gender}{Gender}
 #'   \item{Education}{Education = Five Categories}
 #'   \item{ed}{Education = Two Categories}
-#'}
+#' }
 
 "denial"
+
+#' Contact, threat, and support for Black Lives Matter
+#'
+#' A dataset examining predictors of support for BLM
+#'
+#' @format A data frame with 273 rows and 5 variables:
+#' \describe{
+#'   \item{PosCon}{Positive Contact}
+#'   \item{NegCon}{Negative Contact}
+#'   \item{Real}{Realistic Threat}
+#'   \item{Symb}{Symbolic Threat}
+#'   \item{blm}{Support for Black Lives Matter}
+#'   }
+"BLM"
