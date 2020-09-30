2cf386e3f5a3a0e7b3b1e77c0d6d40df4e4d29c3
chrisaberson <cla18@humboldt.edu>
2020-09-30 16:54:52 -0700

    Add files via upload

--- R/data.R
+++ R/data.R
@@ -32,3 +32,60 @@
 #'   \item{blm}{Support for Black Lives Matter}
 #'   }
 "BLM"
+#' Climate Change Denial
+#'
+#' A dataset examining climate change denial and personality characteristics.
+#' Transgender cases removed
+#'
+#' @format A data frame with 1550 rows and 11 variables:
+#' \describe{
+#'   \item{CCD}{Climate Change Denial}
+#'   \item{ANTIESTABL}{Anti-Establishment Beliefs}
+#'   \item{EXCL_ANTIEG}{Anti-Egalitarian Preferences}
+#'   \item{TRADVALUE}{Traditional Values}
+#'   \item{OPENNESS}{Openness}
+#'   \item{PSEUDOSCI}{Pseudoscience beliefs}
+#'   \item{AGREEABL}{Agreeableness}
+#'   \item{Age}{Age in yeaers}
+#'   \item{Gender}{Gender}
+#'   \item{Education}{Education = Five Categories}
+#'   \item{ed}{Education = Two Categories}
+#' }
+
+"denial2"
+
+#' Yoga for Veterans
+#'
+#' A dataset examining a yoga intervention for vets
+#'
+#' @format A data frame with 27 rows and 2 variables:
+#' \describe{
+#'   \item{preBDI}{Pre-test depression}
+#'   \item{postBDI}{Post-test depression}
+#' }
+
+"condom"
+
+#' Inducing Hypocrisy
+#'
+#' A dataset examining an intervention to increase condom use intentions
+#'
+#' @format A data frame with 16 rows and 2 variables:
+#' \describe{
+#'   \item{CONDITIO}{Condition}
+#'   \item{CONDOM}{Condoms purchased}
+#' }
+
+"condom"
+
+#' Measuring Aggression
+#'
+#' A dataset examining a test of the effects of region and insult on aggresion
+#' @format A data frame with 12 rows and 3 variables:
+#' \describe{
+#'   \item{Region}{Region}
+#'   \item{Condition}{Condition}
+#'   \item{Testosterone}{Testosterone}
+#' }
+
+"culture"
